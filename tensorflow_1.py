17006c926b8eef59df396e03fc6739481480ccaf
hanyiwen <hanyiwen@tju.edu.cn>
2018-10-05 21:39:32 +0800

    update

---
+++ tensorflow_1.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+tf.enable_eager_execution()
+x = tf.get_variable('x', shape=[1], initializer=tf.constant_initializer(3.))
+with tf.GradientTape() as tape:
+    # 在tf.GradientTape() 的上下文内，所有计算步骤都会被记录以用于求导
+    y = tf.square(x)
+    y_grad = tape.gradient(y, x)  # 计算y 关于x 的导数
+print([y.numpy(), y_grad.numpy()])
