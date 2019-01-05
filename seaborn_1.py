bef2de8c508850068423cfb1b212b269dd51e7ce
hanyiwen <hanyiwen@tju.edu.cn>
2019-01-05 22:03:10 +0800

    update

--- seaborn_1.py
+++ seaborn_1.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 #              [20, 20, 20, 12, 5, 3, 0]]
 base_cond = [[1, 2, 3, 4, 5, 4, 7],
              [1, 0, 3, 9, 5, 1, 7],
-             [1, 2, 3, 10, 5, 9, 7]]
+             [3, 2, 3, 10, 5, 9, 7],
+             [1, 2, 7, 10, 4, 9, 7],
+             [1, 2, 3, 6, 5, 2, 7]]
 
 fig = plt.figure()
 
 xdata = np.array([0, 1, 2, 3, 4, 5, 6]) / 5
 
-
-sns.tsplot(time=xdata, data=base_cond, color='g', linestyle='-')
+sns.tsplot(time=xdata, data=base_cond, ci='sd', err_style="ci_band", color='g', linestyle='-')
+# sns.tsplot(time=xdata, data=base_cond, err_style="boot_traces", color='g', linestyle='-')
 
 plt.show()
-
